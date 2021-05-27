df4749bb89149e6ca154ed6e4d6648b25a5b079b
Jonathan Aspeling <jbaspeling@gmail.com>
2021-05-27 20:03:42 +0200

    Spreadsheet Path and initial ETL

--- vuepidgeon/src/App.vue
+++ vuepidgeon/src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
- 
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <p>{{ var1 }}</p>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
+  name: "App",
+  props: [],
+  data() {
+    return {
+      request_url:
+        "https://spreadsheets.google.com/feeds/list/1tdTxwv8j_De1VTbBhYMwC0B3B6_ZdIBgBVOdkxE0FJI/1/public/values?alt=json",
+        shop_item : [],
+        shop_price : [], 
+        department_cat : []
+    };
+  },
+  components: {},
+  methods: {
+    parse_data (json_payload){
 
-  }
-}
+      entries.forEach(
+        (value)=>{
+          temp_record = {
+            "shop_item": value.gsx$winkelitem.t,
+            "shop_price": value.gsx$prys.t,
+            "dep": value.gsx$departement.t
+
+
+          }
+
+        }
+      );
+
+
+
+
+
+
+
+    }
+  },
+  mounted() {
+    var1 = axios
+      .get(request_url)
+      .then((response));
+  },
+};
 </script>
 
 <style>
